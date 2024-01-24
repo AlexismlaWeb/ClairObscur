42bddb0b1adb9e28be8d0706a198d6068f3e8631
AlexismlaWeb <102291173+AlexismlaWeb@users.noreply.github.com>
2024-01-24 17:35:17 +0100

    Update Header.js

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../App.css';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Dropdown, Button } from 'react-bootstrap';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 
 const Header = ({ absolute }) => {
   const navigate = useNavigate();
   const location = useLocation();
 
   const [activePage, setActivePage] = useState(location.pathname);
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 767);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   const handleNavigation = (path) => {
     navigate(path);
     setActivePage(path);
+    setMenuOpen(false);
   };
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 767);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const MenuStyle = {
     position: absolute === true ? 'absolute' : null,
@@ -21,30 +41,45 @@ const Header = ({ absolute }) => {
   return (
     <div className="menu-body" style={MenuStyle}>
       <Row className="align-items-center">
-        <Col sm={2} onClick={() => handleNavigation('/')}>
+        {/* Logo à gauche */}
+        <Col xs={3} md={2} onClick={() => handleNavigation('/')}>
           <img src='./CO_Blanc_SF.png' alt="logo" style={{ width: '100%', cursor: 'pointer' }} />
         </Col>
-        <Col md={{ span: 9, offset: 1 }}>
-          <ul>
-            <li onClick={() => handleNavigation('/')} style={{ opacity: activePage === '/' ? 1 : 0.5 }}>
-              Derniers projets
-            </li>
-            <li
-              onClick={() => handleNavigation('/partenaires')}
-              style={{ opacity: activePage === '/partenaires' ? 1 : 0.5 }}
-            >
-              Nos partenaires
-            </li>
-            <li
-              onClick={() => handleNavigation('/notre-equipe')}
-              style={{ opacity: activePage === '/notre-equipe' ? 1 : 0.5 }}
-            >
-              Notre équipe
-            </li>
-            <li onClick={() => handleNavigation('/contact')} style={{ opacity: activePage === '/' ? 1 : 0.5 }}>
-              Contact
-            </li>
-          </ul>
+        
+        {/* Menu à droite */}
+        <Col xs={8} md={{ span: 9, offset: 1 }}>
+          {isMobileView ? (
+            // Affiche le bouton de menu ou le logo en fonction de la largeur de l'écran
+            <div className="d-flex justify-content-end">
+              <Dropdown show={isMenuOpen} onToggle={handleMenuToggle} align="end">
+                <Dropdown.Toggle as={Button} variant="outline-light" id="dropdown-basic">
+                <FaBars size={25} onClick={handleMenuToggle} style={{ cursor: 'pointer' }} />
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={() => handleNavigation('/')}>Derniers projets</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleNavigation('/partenaires')}>Nos partenaires</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleNavigation('/notre-equipe')}>Notre équipe</Dropdown.Item>
+                  <Dropdown.Item onClick={() => handleNavigation('/contact')}>Contact</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          ) : (
+            // Affiche le menu ul au lieu du bouton de menu
+            <ul className="nav">
+              <li className="nav-item" onClick={() => handleNavigation('/')} style={{ opacity: activePage === '/' ? 1 : 0.5 }}>
+                Derniers projets
+              </li>
+              <li className="nav-item" onClick={() => handleNavigation('/partenaires')} style={{ opacity: activePage === '/partenaires' ? 1 : 0.5 }}>
+                Nos partenaires
+              </li>
+              <li className="nav-item" onClick={() => handleNavigation('/notre-equipe')} style={{ opacity: activePage === '/notre-equipe' ? 1 : 0.5 }}>
+                Notre équipe
+              </li>
+              <li className="nav-item" onClick={() => handleNavigation('/contact')} style={{ opacity: activePage === '/contact' ? 1 : 0.5 }}>
+                Contact
+              </li>
+            </ul>
+          )}
         </Col>
       </Row>
     </div>
